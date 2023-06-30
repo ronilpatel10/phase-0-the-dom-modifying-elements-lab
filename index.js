bf8529357e68bca7c022277cfc0539f0c8aeaacf
Ronil Patel <ronilpatel1001@gmail.com>
2023-06-30 18:08:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Ronil is the champion'; 
+document.body.appendChild(newHeader);
